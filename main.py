42e5315f53024faf1a3a23ef9046e0bb09418708
David Sánchez Mayoral <dsanchez@central.solida.com>
2019-05-20 11:10:49 +0200

    Ejercicio

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
+import sys
 
+class runner():
+    pass
 class Game():
     
     corredores =[]
     
     def __init__(self):
         
-        self.__screen = pygame.display.set_mode((600, 480))
+        self.__screen = pygame.display.set_mode((640, 480))
         pygame.display.set_caption("Carrera de bichos")
         self.background = pygame.image.load("images/background.png")
         
@@ -15,11 +18,12 @@ class Game():
     def competir(self):
         
         x = 0
+        hayGanador = False
         
-        while True:
+        while not hayGanador:
             #comprobación de los eventos
             for event in pygame.event.get():
-                if event.type ==pygame.QUIT:
+                if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                     
@@ -28,6 +32,14 @@ class Game():
             self.__screen.blit(self.background, (0,0))
             self.__screen.blit(self.runner, (x, 240))
             pygame.display.flip()
+            
+            x += 3
+            if x >= 250:
+                hayGanador = True
+                
+        pygame.quit()
+        sys.exit()
+                
         
 if __name__ == "__main__":
     pygame.init()
